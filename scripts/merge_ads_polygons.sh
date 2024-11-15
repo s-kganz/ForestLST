5f9351f72e016803b4738f7a607ed12a12d8ec07
s-kganz <ganzkeenan1@gmail.com>
2024-11-15 23:40:22 +0000

    harmonize legacy/dmsm severity metrics

--- scripts/merge_ads_polygons.sh
+++ scripts/merge_ads_polygons.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 SURVEY_FIELDS="SURVEY_YEAR,IDS_DATA_SOURCE"
-DAMAGE_FIELDS="DAMAGE_AREA_ID,REGION_ID,HOST_CODE,DCA_CODE,SURVEY_YEAR"
-DAMAGE_SQL="DCA_CODE/1000 = 11 OR DCA_CODE/1000 = 15 OR DCA_CODE=50003"
 INPUT_DIRECTORY=data_in/ads
 OUTPUT_DIRECTORY=data_working
 DAMAGE_TEMP=$INPUT_DIRECTORY/damage_temp
@@ -16,16 +14,21 @@ for f in $INPUT_DIRECTORY/*.gdb; do
     region=${bname//[^0-9]/}
     damage_layer=DAMAGE_AREAS_FLAT_AllYears_CONUS_Rgn${region}
     survey_layer=SURVEYED_AREAS_FLAT_AllYears_CONUS_Rgn${region}
+
+    damage_sql="SELECT SHAPE,SURVEY_YEAR,PERCENT_MID,ifnull(PERCENT_MID, 100) AS SEVERITY FROM $damage_layer WHERE DCA_CODE/1000 = 11 OR DCA_CODE/1000 = 15 OR DCA_CODE=50003"
     
-    ogr2ogr $DAMAGE_TEMP/$bname $f $damage_layer -select $DAMAGE_FIELDS -where "$DAMAGE_SQL"
+    ogr2ogr $DAMAGE_TEMP/$bname $f -dialect SQLite -sql "$damage_sql" -nln damage
     ogr2ogr $SURVEY_TEMP/$bname $f $survey_layer -select $SURVEY_FIELDS
 done
 
 damage_files=$(find $DAMAGE_TEMP -maxdepth 1 -mindepth 1)
 survey_files=$(find $SURVEY_TEMP -maxdepth 1 -mindepth 1)
 
-ogrmerge -o $OUTPUT_DIRECTORY/damage_merged.gdb $damage_files -single
-ogrmerge -o $OUTPUT_DIRECTORY/survey_merged.gdb $survey_files -single
+echo $damage_files
+echo $survey_files
+
+ogrmerge -o $OUTPUT_DIRECTORY/damage_merged.gdb $damage_files -single -overwrite_ds
+ogrmerge -o $OUTPUT_DIRECTORY/survey_merged.gdb $survey_files -single -overwrite_ds
 
 rm -rf $DAMAGE_TEMP
 rm -rf $SURVEY_TEMP
