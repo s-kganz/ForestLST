0a866af9676cdbae6605f40525611629b7444587
s-kganz <ganzkeenan1@gmail.com>
2025-02-11 17:26:30 +0000

    reorganize util

--- util/__init__.py
+++ util/__init__.py
@@ -4,9 +4,11 @@ import os
 from . import data
 from . import daymet
 from . import plot
+from . import preisler
+from . i
 
 try:
-    from . import training_torch as training
+    from . import training
     from . import convlstm
 except ImportError:
     warnings.warn("PyTorch not found! Module util.training will not be loaded.")
