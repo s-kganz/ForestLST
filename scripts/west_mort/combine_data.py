5ff9a480a4ab9e07bd5d867e6c040a286e7bfb65
s-kganz <ganzkeenan1@gmail.com>
2025-03-13 20:52:44 +0000

    force coords to match in combine_data to prevent floating point imprecision issues

--- scripts/west_mort/combine_data.py
+++ scripts/west_mort/combine_data.py
@@ -4,12 +4,9 @@ import rasterio
 import numpy as np
 import dask
 import os
-from dask.distributed import Client
+dask.config.set(scheduler="synchronous")
 #client = Client()
 #client
-dask.config.set(scheduler="synchronous")
-
-CHUNK_SCHEMA = dict(time=27, x=512, y=512)
 CA_BOUNDS_3857 = (-13846000.0, 3842000.0, -12950000.0, 5162000.0)
 
 # Define several functions to parse intermediate data
@@ -42,57 +39,83 @@ def preprocess_ba(ds):
     return ds.squeeze(drop=True).rename(band_data=genus)
 
 if __name__ == "__main__":
-    # Read in all the data
+    # Read in all the data.
+    # Floating point imprecision leads to *very* slight differences
+    # in x and y coordinates among the intermediate data. BUT they all
+    # have the same width/eheight. So we assign x/y coords based on mortality
+    # data.
     damage = xr.open_mfdataset(
         "data_working/damage_rasters/*.tif",
         preprocess=preprocess_mortality,
         concat_dim="time", 
         combine="nested"
-    ).chunk(**CHUNK_SCHEMA)
+    )
     
     fam = xr.open_mfdataset(
         "data_working/fam_rasters/*.tif",
         preprocess=preprocess_fam,
         concat_dim="time", 
         combine="nested"
-    ).chunk(**CHUNK_SCHEMA)
+    ).assign_coords(
+        x=damage.x,
+        y=damage.y
+    )
 
     treecover = xr.open_dataset("data_working/forest_cover.tif")\
         .squeeze(drop=True)\
         .drop_vars(["spatial_ref"])\
         .rename(band_data="treecover")\
         .expand_dims(time=damage.time.shape[0])\
-        .assign_coords(time=damage.time)\
-        .chunk(**CHUNK_SCHEMA)
+        .assign_coords(
+            time=damage.time,
+            x=damage.x,
+            y=damage.y
+        )
 
     daymet = xr.open_mfdataset('data_working/daymet/*.nc', concat_dim="time", combine="nested")\
         .drop_vars("spatial_ref")\
-        .chunk(**CHUNK_SCHEMA)
+        .assign_coords(
+            x=damage.x,
+            y=damage.y
+        )
 
     fire = xr.open_mfdataset(
         "data_working/mtbs_rasters/*.tif",
         preprocess=preprocess_fire,
         concat_dim="time", combine="nested"
-    ).chunk(**CHUNK_SCHEMA)
+    ).assign_coords(
+        x=damage.x,
+        y=damage.y
+    )
 
     terrain = xr.open_dataset("data_working/terrain.nc")\
         .drop_vars(["spatial_ref", "band"])\
         .expand_dims(time=damage.time.shape[0])\
-        .assign_coords(time=damage.time)\
-        .chunk(**CHUNK_SCHEMA)
+        .assign_coords(
+            time=damage.time,
+            x=damage.x,
+            y=damage.y
+        )
 
     vod = xr.open_dataset("data_working/summer_vod.nc")\
-        .drop_vars("spatial_ref")\
-        .chunk(**CHUNK_SCHEMA)
-    vod = vod.assign_coords(time=vod.time.dt.year)
+        .drop_vars("spatial_ref")
+    
+    vod = vod.assign_coords(
+        time=vod.time.dt.year,
+        x=damage.x,
+        y=damage.y
+    )
 
     genus_ba = xr.open_mfdataset(
         "data_working/genus_basal_area/*.tif",
         preprocess=preprocess_ba
     )\
         .expand_dims(time=damage.time.shape[0])\
-        .assign_coords(time=damage.time)\
-        .chunk(**CHUNK_SCHEMA)
+        .assign_coords(
+            time=damage.time,
+            x=damage.x,
+            y=damage.y
+        )
 
     # Merge it
     all_data = xr.combine_by_coords(
