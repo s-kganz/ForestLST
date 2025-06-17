d930e8110ebd5ac7009536dd81552e34bb06ff79
s-kganz <ganzkeenan1@gmail.com>
2025-06-17 20:39:52 +0000

    reproject ads polygons in combine script

--- scripts/west_mort/merge_ads_polygons.sh
+++ scripts/west_mort/merge_ads_polygons.sh
@@ -17,7 +17,7 @@ for f in $INPUT_DIRECTORY/*.gdb; do
 
     # If severity was not recorded, gapfill with 10%. This corresponds to roughly the mean of observations with a 
     # recorded severity value from 2012 - 2023.
-    damage_sql="SELECT SHAPE,SURVEY_YEAR,DCA_CODE,DAMAGE_TYPE_CODE,PERCENT_MID,ifnull(PERCENT_MID, 10) AS SEVERITY FROM $damage_layer WHERE ST_Area(SHAPE) > 10000 AND (DAMAGE_TYPE_CODE = 2 OR DAMAGE_TYPE_CODE = 11) AND (DCA_CODE/1000 = 11 OR DCA_CODE/1000 = 15 OR DCA_CODE=50003)"
+    damage_sql="SELECT SHAPE,SURVEY_YEAR,DCA_CODE,DAMAGE_TYPE_CODE,PERCENT_MID,ifnull(PERCENT_MID, 10) AS SEVERITY FROM $damage_layer WHERE ST_Area(SHAPE) > 10000 AND (DAMAGE_TYPE_CODE = 2 OR DAMAGE_TYPE_CODE = 11) AND (DCA_CODE/1000 = 11 OR DCA_CODE/1000 = 15 OR DCA_CODE=50003) ORDER BY SEVERITY"
     
     ogr2ogr $DAMAGE_TEMP/$bname $f -dialect SQLite -sql "$damage_sql" -nln damage -overwrite
     ogr2ogr $SURVEY_TEMP/$bname $f $survey_layer -select $SURVEY_FIELDS -overwrite
@@ -29,8 +29,8 @@ survey_files=$(find $SURVEY_TEMP -maxdepth 1 -mindepth 1)
 echo $damage_files
 echo $survey_files
 
-ogrmerge -o $OUTPUT_DIRECTORY/damage_merged.gdb $damage_files -single -overwrite_ds -progress -s_srs "ESRI:102039" -t_srs "EPSG:3857"
-ogrmerge -o $OUTPUT_DIRECTORY/survey_merged.gdb $survey_files -single -overwrite_ds -progress -s_srs "ESRI:102039" -t_srs "EPSG:3857"
+ogrmerge -o $OUTPUT_DIRECTORY/damage_merged.gdb $damage_files -single -overwrite_ds -progress -s_srs "ESRI:102039" -t_srs $1
+ogrmerge -o $OUTPUT_DIRECTORY/survey_merged.gdb $survey_files -single -overwrite_ds -progress -s_srs "ESRI:102039" -t_srs $1
 
 rm -rf $DAMAGE_TEMP
 rm -rf $SURVEY_TEMP
